e2cc6bf55153a752fe40af58c55ec22bb98a2825
croc0boy <sunat.rustamov1@gmail.com>
2022-04-28 21:08:48 +0500

    Hi

--- app/src/androidTest/java/uz/direction/noteapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/uz/direction/noteapp/ExampleInstrumentedTest.kt
@@ -18,6 +18,7 @@ class ExampleInstrumentedTest {
     @Test
     fun useAppContext() {
         // Context of the app under test.
+
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
         assertEquals("uz.direction.noteapp", appContext.packageName)
         ///asdasda
