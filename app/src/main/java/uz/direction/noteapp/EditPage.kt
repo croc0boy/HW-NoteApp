b82537e19648e8181c1b2d9ef68ce8acc340b56f
croc0boy <sunat.rustamov1@gmail.com>
2022-05-25 21:05:23 +0500

    Add shared preferences, fixed an issue with the button

--- app/src/main/java/uz/direction/noteapp/EditPage.kt
+++ app/src/main/java/uz/direction/noteapp/EditPage.kt
@@ -1,11 +1,13 @@
 package uz.direction.noteapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
+import androidx.fragment.app.setFragmentResult
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 
@@ -15,9 +17,16 @@ class EditPage : Fragment(R.layout.fragment_edit_page) {
     private val args by navArgs<EditPageArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val bundle: Bundle = Bundle()
+
         super.onViewCreated(view, savedInstanceState)
         val textEdit = view.findViewById<EditText>(R.id.text)
         val titleEdit = view.findViewById<EditText>(R.id.title)
+
+        val sharedPreferences = activity?.getSharedPreferences(
+            getString(R.string.shared_pref_key),
+            Context.MODE_PRIVATE
+        )
 
         view.findViewById<ImageButton>(R.id.button_done).setOnClickListener {
             val action = EditPageDirections.actionEditPageToFirstPage(
@@ -25,16 +34,20 @@ class EditPage : Fragment(R.layout.fragment_edit_page) {
                 titleChanged = titleEdit.text.toString()
             )
 
+            bundle.putString(getString(R.string.title), titleEdit.text.toString())
+            bundle.putString(getString(R.string.text),  textEdit.text.toString())
+
+            sharedPreferences?.edit()?.apply() {
+                putString(getString(R.string.title), titleEdit.toString())
+                putString(getString(R.string.text), textEdit.toString())
+                apply()
+            }
+            setFragmentResult("fragmentKey", bundle)
             findNavController().navigate(action)
         }
         textEdit.setText(args.text)
         titleEdit.setText(args.title)
 
-
-
-
     }
-
-
 
 }
--- app/src/main/java/uz/direction/noteapp/FirstPage.kt
+++ app/src/main/java/uz/direction/noteapp/FirstPage.kt
@@ -1,5 +1,8 @@
 package uz.direction.noteapp
 
+import android.app.Activity
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,12 +11,15 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import uz.direction.noteapp.databinding.FragmentFirstPageBinding
 
 
 class FirstPage : Fragment(R.layout.fragment_first_page) {
-
+    private var binding: FragmentFirstPageBinding? = null
     private val args by navArgs<FirstPageArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -21,15 +27,40 @@ class FirstPage : Fragment(R.layout.fragment_first_page) {
         val text = view.findViewById<TextView>(R.id.text)
         val title = view.findViewById<TextView>(R.id.title)
         view.findViewById<ImageButton>(R.id.button_edit).setOnClickListener {
-        val action = FirstPageDirections.actionFirstPageToEditPage(
-            text = text.text.toString(),
-            title = title.text.toString())
+            val action = FirstPageDirections.actionFirstPageToEditPage(
+                text = text.text.toString(),
+                title = title.text.toString()
+            )
             findNavController().navigate(action)
         }
-
         text.text = args.textChanged
         title.text = args.titleChanged
+
+        val sharedPreferences = activity?.getSharedPreferences(
+            getString(R.string.shared_pref_key),
+            Context.MODE_PRIVATE
+        )
+        binding?.title?.text =
+            sharedPreferences?.getString(getString(R.string.title), "Write title here!")
+        binding?.text?.text =
+            sharedPreferences?.getString(getString(R.string.text), "Write text here!")
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        setFragmentResultListener("fragmentKey") { fragmentKey, bundle ->
+            binding?.text?.text = bundle.getString("text")
+            binding?.title?.text = bundle.getString("title")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFirstPageBinding.inflate(layoutInflater)
+        return binding!!.root
+    }
 }
